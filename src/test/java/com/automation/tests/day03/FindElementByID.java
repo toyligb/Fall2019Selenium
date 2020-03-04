4f64ce8789e2d5ac1d19469b91b0f49140b3fe31
Toyly Gurdov <toyligb@gmail.com>
2020-03-03 19:04:40 -0800

    WebElement linkText

--- src/test/java/com/automation/tests/day03/FindElementByID.java
+++ src/test/java/com/automation/tests/day03/FindElementByID.java
@@ -14,6 +14,7 @@ public class FindElementByID {
 //        WebDriverManager.chromedriver().setup();
 //        WebDriver driver = new ChromeDriver();
         WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.manage().window().maximize();
         driver.get("http://practice.cybertekschool.com/login");
         driver.findElement(By.name("username")).sendKeys("tomsmith");
         Thread.sleep(2000);
@@ -32,9 +33,30 @@ public class FindElementByID {
 
         // Let's click on Logout button. It looks like a button, but it's actually a link
         // every element with <a> tag is a link
-
+        // if you have couple spaces in the text, just use partialLinkText instead of linkText
+        // linkText -->> equals()
+        // partialLinkText -->> contains() - complete match doesn't required
         WebElement logout = driver.findElement(By.linkText("Logout"));
+
+        String href = logout.getAttribute("href");
+        String className = logout.getAttribute("class");
+
+        System.out.println("href = " + href);
+        System.out.println("className = " + className);
+
         logout.click();
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.name("username")).sendKeys("toyligb");
+        Thread.sleep(2000);
+        driver.findElement(By.name("password")).sendKeys("asdfv");
+        Thread.sleep(2000);
+        driver.findElement(By.id("wooden_spoon")).click();
+        Thread.sleep(3000);
+
+        WebElement errorMessage = driver.findElement(By.id("flash-messages"));
+        System.out.println(errorMessage.getText());
         Thread.sleep(3000);
 
         driver.quit();
