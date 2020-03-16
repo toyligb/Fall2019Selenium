3e6ca9c150fdd6f7bbc950b889f3ba127e8e9478
Toyly Gurdov <toyligb@gmail.com>
2020-03-15 18:30:47 -0700

    day09 vytrack

---
+++ src/test/java/com/automation/tests/day08/UnitTestPractice.java
@@ -0,0 +1,66 @@
+package com.automation.tests.day08;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UnitTestPractice {
+
+    public static void main(String[] args) {
+        //unit test
+        //to check if our method works properly
+        //if assertion fails, that means our method doesn't work correctly
+        //that means we have fix the method
+        String expected = "cba";
+        String toReverse = "abc";
+        String actual = reverseString(toReverse);
+        //Assertion
+        verifyEquals(expected, actual);
+
+    }
+
+    //annotation
+    //description - is not working for junit, make sure that you use testng
+    @Test(description = "Verify if method can reverse a string")
+    public void test() {
+        String expected = "elpp";
+        String actual = reverseString("apple");
+        //it coming from testng, junit also has this class
+        //you can compare any data types here: strings, primitives, arrays, objects
+        //to verify if expected result is equals to actual
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(description = "Verify if method can reverse a string")
+    public void test2() {
+        String expected = "rac";
+        String actual = reverseString("car");
+        Assert.assertEquals(actual, expected);
+    }
+
+    public static boolean verifyEquals(String expected, String actual) {
+        if (expected.equals(actual)) {
+            System.out.println("TEST PASSED");
+            return true;
+        } else {
+            System.out.println("Test failed!!!");
+            System.out.println("Expected: " + expected);
+            System.out.println("Actual: " + actual);
+            return false;
+        }
+    }
+
+    /**
+     * This method stands for reversing strings.
+     *
+     * @param str to reverse
+     * @return reversed string
+     */
+    public static String reverseString(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
+    }
+
+}
